8ac2b0b82aef4f59c19c480662d93fb6467d1181
vhhg <qieka_zhang@yeah.net>
2023-04-30 15:45:44 +0800

    feat 配置gulp自动打包配置文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,56 @@
-const gulp = require('gulp')
-// 获取 cleancss 模块（用于压缩 CSS）
-const uglify = require('gulp-uglify');
-const cleanCSS = require('gulp-clean-css')
-const rename = require('gulp-rename')
-const concat = require('gulp-concat')
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const cssnano = require("gulp-cssnano");
+const autoprefixer = require("gulp-autoprefixer");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+
+// # 通用的CSS压缩
+
+gulp.task("css", () => {
+  return gulp
+    .src("src/css/**/*.css")
+    .pipe(concat("eltos.css"))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(cssnano())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("dist/css"));
+});
+
+gulp.task("layout", () => {
+  return gulp
+    .src("src/css/common/layout.css")
+    .pipe(concat("layout.css"))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(cssnano())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("dist/css"));
+});
+
+gulp.task("default", gulp.parallel("css", "layout"));
 
 // 压缩js文件
 // gulp.task('jsCompress', function() {
 //  return gulp.src('js/1.js').pipe(uglify()).pipe(gulp.dest('dist/js'))
 // });
 
-// 压缩css文件
-gulp.task('cssCompress', function () {
-	return gulp.src('libraries/style/reset.css')
-  // return gulp.src('libraries/style/flex.css')
-    .pipe(cleanCSS())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('libraries/dist/css'))
-});
+// // 压缩css文件
+// gulp.task('cssCompress', function () {
+// 	return gulp.src('libraries/style/reset.css')
+//   // return gulp.src('libraries/style/flex.css')
+//     .pipe(cleanCSS())
+//     .pipe(rename({suffix: '.min'}))
+//     .pipe(gulp.dest('libraries/dist/css'))
+// });
 
 // gulp.task('cssCompress', function () {
 //   return gulp.src('css/*.css')
